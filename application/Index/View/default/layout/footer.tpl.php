69a9a41d82e41af253b874cbdc8a8070309deaf1
Heanes <heanes@163.com>
2018-01-14 23:01:50 +0800

    add baidu statistics

--- application/Index/View/default/layout/footer.tpl.php
+++ application/Index/View/default/layout/footer.tpl.php
@@ -6,4 +6,16 @@
  */
 defined('InHeanes') or die('Access denied!');
 ?>
+<script type="text/javascript">
+    // S 百度统计
+    var _hmt = _hmt || [];
+    (function() {
+        var hm = document.createElement("script");
+        hm.src = "https://hm.baidu.com/hm.js?2ac84f39bf32fc0a8398306abe141608";
+        var s = document.getElementsByTagName("script")[0];
+        s.parentNode.insertBefore(hm, s);
+    })();
+    // E 百度统计
+</script>
+
 
